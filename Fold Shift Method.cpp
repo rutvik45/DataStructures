54c5797db3a23e0923c3783ad569a2295a61dc3b
Rutvik Dhananjay Redkar <70567390+rutvik45@users.noreply.github.com>
2023-02-17 18:36:52 +0530

    Create Fold Shift Method.cpp

---
+++ Fold Shift Method.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+using namespace std;
+const int T_S = 5;
+class HashTable {
+public:
+int k;
+int v;
+HashTable(int k, int v) {
+this->k = k;
+this->v = v;
+}
+};
+class DelNode:public HashTable {
+private:
+static DelNode *en;
+DelNode():HashTable(-1, -1) {}
+public:
+static DelNode *getNode() {
+if (en == NULL)
+en = new DelNode();
+return en;
+
+}
+};
+DelNode *DelNode::en = NULL;
+class HashMapTable {
+private:
+HashTable **ht;
+public:
+HashMapTable() {
+ht = new HashTable* [T_S];
+for (int i = 0; i < T_S; i++) {
+ht[i] = NULL;
+}
+}
+int HashFunc(int k) {
+return k % T_S;
+}
+
+void Insert(int k, int v) {
+int hash_val = HashFunc(k);
+int init = -1;
+int delindex = -1;
+while (hash_val != init && (ht[hash_val] == DelNode::getNode() || ht[hash_val] !=
+NULL && ht[hash_val]->k != k)) {
+if (init == -1)
+init = hash_val;
+if (ht[hash_val] == DelNode::getNode())
+delindex = hash_val;
+hash_val = HashFunc(hash_val + 1);
+}
+if (ht[hash_val] == NULL || hash_val == init) {
+if(delindex != -1)
+ht[delindex] = new HashTable(k, v);
+else
+ht[hash_val] = new HashTable(k, v);
+}
+if(init != hash_val) {
+if (ht[hash_val] != DelNode::getNode()) {
+if (ht[hash_val] != NULL) {
+if (ht[hash_val]->k== k)
+ht[hash_val]->v = v;
+}
+} else
+ht[hash_val] = new HashTable(k, v);
+}
+}
+int SearchKey(int k) {
+
+int hash_val = HashFunc(k);
+int init = -1;
+while (hash_val != init && (ht[hash_val] == DelNode::getNode() || ht[hash_val] != NULL
+&& ht[hash_val]->k!= k)) {
+if (init == -1)
+init = hash_val;
+hash_val = HashFunc(hash_val + 1);
+}
+if (ht[hash_val] == NULL || hash_val == init)
+return -1;
+else
+return ht[hash_val]->v;
+}
+void Remove(int k) {
+int hash_val = HashFunc(k);
+int init = -1;
+while (hash_val != init && (ht[hash_val] == DelNode::getNode() || ht[hash_val] != NULL
+&& ht[hash_val]->k!= k)) {
+if (init == -1)
+init = hash_val;
+hash_val = HashFunc(hash_val + 1);
+}
+if (hash_val != init && ht[hash_val] != NULL) {
+delete ht[hash_val];
+ht[hash_val] = DelNode::getNode();
+}
+}
+~HashMapTable() {
+delete[] ht;
+}
+};
+int main() {
+HashMapTable hash;
+int k, v;
+int c,f,l,kv;
+cout<<"\n--------------------------------\n";
+cout<<"ASHVIN SHETTY - FY-MCA-B-104 \n";
+cout<<"\n--------------------------------\n";
+cout<<"\nDigit Extraction Technique with Linear Probe\n";
+cout<<"\n--------------------------------\n";
+while(1) {
+cout<<"1.Insert element into the table"<<endl;
+cout<<"2.Search element from the key"<<endl;
+cout<<"3.Delete element at a key"<<endl;
+cout<<"4.Exit"<<endl;
+cout<<"Enter your choice: ";
+cin>>c;
+switch(c) {
+
+case 1:
+cout<<"Enter element to be inserted in the range 100-999: ";
+cin>>v;
+f = ((v % 1000)/10)/10;
+l = v%10;
+kv = (f*10)+l;
+//key manipulation
+cout<<"\nGenerated Key: "<<kv<<"\n";
+hash.Insert(kv, v);
+break;
+case 2:
+cout<<"Enter the element to be searched: ";
+cin>>k;
+if(hash.SearchKey(kv) == -1) {
+cout<<"No element found at key "<<k<<endl;
+continue;
+} else {
+cout<<"Element at key "<<kv<<" : ";
+cout<<hash.SearchKey(kv)<<endl;
+}
+break;
+case 3:
+cout<<"Enter key of the element to be deleted: ";
+cin>>k;
+hash.Remove(kv);
+break;
+case 4:
+exit(1);
+default:
+cout<<"\nEnter correct option\n";
+}
+}
+return 0;
+}
